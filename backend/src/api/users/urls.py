b6538e797d9c10872d6bdeda4a53c8ad77736a55
AkashSDas <aakashdas368@gmail.com>
2024-04-07 11:25:27 +0530

    [be] add logout

--- backend/src/api/users/urls.py
+++ backend/src/api/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from api.users.views import signup_view
+from api.users.views import signup_view, logout_view
 
 
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="access-token"),
     path("login/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
+    path("logout/", logout_view, name="logout"),
 ]
--- backend/src/api/users/views.py
+++ backend/src/api/users/views.py
@@ -1,11 +1,16 @@
 from typing import cast
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from api.users.serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from api.users.models import User
-from rest_framework.status import HTTP_201_CREATED
+from rest_framework.status import (
+    HTTP_201_CREATED,
+    HTTP_205_RESET_CONTENT,
+    HTTP_400_BAD_REQUEST,
+)
 
 
 def get_tokens_for_user(user: User) -> dict:
@@ -31,3 +36,21 @@ def signup_view(req: Request, *args, **kwargs) -> Response:
         },
         status=HTTP_201_CREATED,
     )
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def logout_view(req: Request, *args, **kwargs) -> Response:
+    try:
+        if not isinstance(req.data, dict):
+            return Response(
+                {"error": "Invalid input. Expected JSON data"},
+                status=HTTP_400_BAD_REQUEST,
+            )
+
+        refresh_token = req.data.get("refresh")
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({"message": "Logout successful"}, status=HTTP_205_RESET_CONTENT)
+    except Exception as e:
+        return Response({"error": str(e)}, status=HTTP_400_BAD_REQUEST)
