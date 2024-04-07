0afbf968a080a3d72eb44c7a7a7860690112abf8
AkashSDas <aakashdas368@gmail.com>
2024-04-07 14:44:21 +0530

    [fe] remove frontend dir

--- frontend/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "@app/styles/globals.css";
-import type { AppProps } from "next/app";
-
-export default function App(props: AppProps): React.JSX.Element {
-    const { Component, pageProps } = props;
-    return <Component {...pageProps} />;
-}
--- frontend/src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomePage(): React.JSX.Element {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
