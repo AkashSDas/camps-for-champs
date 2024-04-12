ec333ea80097f44fb7c1616d3dd9aca9703b8d8a
AkashSDas <aakashdas368@gmail.com>
2024-04-12 08:44:53 +0530

    [be] create tag model

--- backend/src/api/tags/models.py
+++ backend/src/api/tags/models.py
@@ -1,3 +1,31 @@
+from typing import Any
 from django.db import models
+from django.forms import ValidationError
 
-# Create your models here.
+
+def validate_label(value: Any):
+    if not isinstance(value, str):
+        raise ValidationError("Label must be a string")
+    if len(value.strip()) < 3:
+        raise ValidationError("Label is too short")
+
+
+class Tag(models.Model):
+    label = models.CharField(
+        unique=True,
+        blank=False,
+        null=False,
+        max_length=100,
+        db_index=True,
+        verbose_name="Tag label",
+        help_text="Enter a tag label",
+        validators=[validate_label],
+    )
+
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+        ordering = ["label"]
