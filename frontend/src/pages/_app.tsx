f42fc755d4b457cdcb3de971c91e845652ee6406
AkashSDas <aakashdas368@gmail.com>
2024-04-18 09:49:09 +0530

    [fe] setup mui and gsap

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@app/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Bungee, Sora } from "next/font/google";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "@app/lib/styles";
+
+const headingFont = Bungee({ weight: ["400"], subsets: ["latin"] });
+const bodyFont = Sora({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div className={`${headingFont.className} ${bodyFont.className}`}>
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+                <CssBaseline />
+            </ThemeProvider>
+        </div>
+    );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <h1>Camps for Champs</h1>;
+    return (
+        <>
+            <Head>
+                <title>Camps for Champs</title>
+                <meta name="description" content="Camps for Champs" />
+                <link rel="icon" href="/logo-icon.ico" />
+            </Head>
+
+            <main>
+                <h1>Camps for Champs</h1>
+            </main>
+        </>
+    );
 }
