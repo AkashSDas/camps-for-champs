c1c2e7ab4bd9970b1ebae7515663e89c41fec842
AkashSDas <aakashdas368@gmail.com>
2024-04-20 11:26:12 +0530

    [fe] add query client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,17 +4,21 @@ import { Bungee, Sora } from "next/font/google";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@app/lib/styles";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@app/lib/react-query";
 
 export const headingFont = Bungee({ weight: ["400"], subsets: ["latin"] });
 export const bodyFont = Sora({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <div className={`${headingFont.className} ${bodyFont.className}`}>
-            <ThemeProvider theme={theme}>
-                <Component {...pageProps} />
-                <CssBaseline />
-            </ThemeProvider>
-        </div>
+        <QueryClientProvider client={queryClient}>
+            <div className={`${headingFont.className} ${bodyFont.className}`}>
+                <ThemeProvider theme={theme}>
+                    <Component {...pageProps} />
+                    <CssBaseline />
+                </ThemeProvider>
+            </div>
+        </QueryClientProvider>
     );
 }
