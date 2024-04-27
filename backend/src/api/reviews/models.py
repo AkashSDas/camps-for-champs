9ec6031773f9f67e2d368b9c7e58e12a4c2c53ee
AkashSDas <aakashdas368@gmail.com>
2024-04-27 10:26:44 +0530

    [be] add migration for altering CampImage's image field and update Review model with custom querysets and managers

--- backend/src/api/reviews/models.py
+++ backend/src/api/reviews/models.py
@@ -13,6 +13,57 @@ class Rating(Enum):
     FIVE = 5
 
 
+class CampRelatedReviewQueryset(models.QuerySet):
+    def avg_rating(self, camp_id: int):
+        return self.filter(camp_id=camp_id).aggregate(models.Avg("rating"))
+
+    def camp_top_rating(self, camp_id: int):
+        return self.filter(camp_id=camp_id).order_by("-rating", "-helpful_count")
+
+    def top_rated_camps(self):
+        return (
+            self.values("camp")
+            .annotate(
+                avg_rating=models.Avg("rating"),
+                total_reviews=models.Count("id"),
+            )
+            .order_by("-avg_rating", "-total_reviews")
+        )
+
+
+class ReviewQuerySet(models.QuerySet):
+    def public(self):
+        return self.filter(is_public=True)
+
+    def helpful(self):
+        return self.filter(helpful_count__gte=1)
+
+    def reported(self):
+        return self.filter(report_review_count__gte=1)
+
+
+class AuthorReviewManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def author_reviews(self, author_id: int):
+        return self.get_queryset().filter(author_id=author_id).order_by("-created_at")
+
+
+class ReviewManager(models.Manager):
+    def get_queryset(self):
+        return ReviewQuerySet(self.model, using=self._db)
+
+    def public(self):
+        return self.get_queryset().public()
+
+    def helpful(self):
+        return self.get_queryset().helpful()
+
+    def reported(self):
+        return self.get_queryset().reported()
+
+
 class Review(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     camp = models.ForeignKey(Camp, on_delete=models.CASCADE)
@@ -28,3 +79,7 @@ class Review(models.Model):
         db_table = "reviews"
         ordering = ["-created_at"]
         unique_together = ["author", "camp"]  # author can only review a camp once
+
+    objects = ReviewManager()
+    camps = CampRelatedReviewQueryset.as_manager()
+    authors = AuthorReviewManager()
