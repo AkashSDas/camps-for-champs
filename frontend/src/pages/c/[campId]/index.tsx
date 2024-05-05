68290218be979821b4e36f5c7b5a0a55e66bb753
AkashSDas <aakashdas368@gmail.com>
2024-05-05 11:22:57 +0530

    [fe] add pages boilerplate

---
+++ frontend/src/pages/c/[campId]/index.tsx
@@ -0,0 +1,7 @@
+export default function CampInfo() {
+    return (
+        <div>
+            <h1>Camp info</h1>
+        </div>
+    );
+}
---
+++ frontend/src/pages/c/[campId]/reviews.tsx
@@ -0,0 +1,7 @@
+export default function CampReviews() {
+    return (
+        <div>
+            <h1>Camp Reviews</h1>
+        </div>
+    );
+}
