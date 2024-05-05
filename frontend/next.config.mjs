5c64adcc09dfb588e313ea2033593cf91d9d9900
AkashSDas <aakashdas368@gmail.com>
2024-05-05 08:51:44 +0530

    [fe] add assets, pkgs, and remove global anchor tag default style

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+                pathname: "/akashsdas/image/upload/v1/camps-for-champs/**",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
