a789e5aa46bb2cab55276a67f570ae814cd6ff47
AkashSDas <aakashdas368@gmail.com>
2024-05-31 09:36:41 +0530

    [fe] wrap stripe in the root

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -12,27 +12,23 @@ import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
 export const headingFont = Bungee({ weight: ["400"], subsets: ["latin"] });
 export const bodyFont = Sora({ subsets: ["latin"] });
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-// const stripePromise = loadStripe(process.env.NEXT_STRIPE_PUBLISHABLE_KEY!);
-
-// TODO: use Element in payment form wrapper
+const stripePromise = loadStripe(process.env.NEXT_STRIPE_PUBLISHABLE_KEY!);
 
 export default function App({ Component, pageProps }: AppProps) {
-    // const options: StripeElementsOptions = {
-    //     clientSecret: process.env.NEXT_STRIPE_CLIENT_SECRET!, payment intent
-    // };
+    const options: StripeElementsOptions = {};
 
     return (
-        // <Elements stripe={stripePromise} options={options}>
-        <QueryClientProvider client={queryClient}>
-            <div className={`${headingFont.className} ${bodyFont.className}`}>
-                <ThemeProvider theme={theme}>
-                    <Component {...pageProps} />
-                    <CssBaseline />
-                </ThemeProvider>
-            </div>
-        </QueryClientProvider>
-        // </Elements>
+        <Elements stripe={stripePromise} options={options}>
+            <QueryClientProvider client={queryClient}>
+                <div
+                    className={`${headingFont.className} ${bodyFont.className}`}
+                >
+                    <ThemeProvider theme={theme}>
+                        <Component {...pageProps} />
+                        <CssBaseline />
+                    </ThemeProvider>
+                </div>
+            </QueryClientProvider>
+        </Elements>
     );
 }
