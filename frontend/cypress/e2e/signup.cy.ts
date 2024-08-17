80be489b7931e1edfd8078f94dac0c80e8310239
AkashSDas <aakashdas368@gmail.com>
2024-08-17 10:51:59 +0530

    [fe] use aliases in signup cypress test

--- frontend/cypress/e2e/signup.cy.ts
+++ frontend/cypress/e2e/signup.cy.ts
@@ -1,38 +1,55 @@
 describe("Signup", () => {
     beforeEach(() => {
         cy.visit("/");
+
+        cy.get("[data-test='signup-button']").as("openModalButton");
     });
 
-    describe("when a user is not logged in", () => {
-        it("should open signup modal form when signup button is clicked", () => {
-            cy.get("[data-test='signup-button']").click();
-            cy.get("[data-test='signup-modal']").should("be.visible");
-            cy.get("[data-test='signup-form']").should("be.visible");
-            cy.get("[data-test='signup-email-input']").should("be.visible");
-            cy.get("[data-test='signup-firstname-input']").should("be.visible");
-            cy.get("[data-test='signup-lastname-input']").should("be.visible");
-            cy.get("[data-test='signup-password-input']").should("be.visible");
-            cy.get("[data-test='signup-submit-button']").should("be.visible");
+    it("should contain signup button", () => {
+        cy.get("@openModalButton").should("be.visible");
+    });
+
+    describe("when the modal is opened", () => {
+        beforeEach(() => {
+            cy.get("@openModalButton").click();
+
+            cy.get("[data-test='signup-modal']").as("modal");
+            cy.get("[data-test='signup-form']").as("form");
+            cy.get("[data-test='signup-email-input']").as("emailInput");
+            cy.get("[data-test='signup-firstname-input']").as("firstNameInput");
+            cy.get("[data-test='signup-lastname-input']").as("lastNameInput");
+            cy.get("[data-test='signup-password-input']").as("passwordInput");
+            cy.get("[data-test='signup-submit-button']").as("formSubmitBtn");
         });
 
-        it("should show error message when signup form is submitted with invalid data", () => {
-            cy.get("[data-test='signup-button']").click();
-            cy.get("[data-test='signup-form']").submit();
-            cy.get("[data-test='signup-email-input'] > .helper-text").should(
-                "be.visible"
-            );
-            cy.get("[data-test='signup-password-input'] > .helper-text").should(
-                "be.visible"
-            );
+        it("should show signup form", () => {
+            cy.get("@modal").should("be.visible");
+            cy.get("@form").should("be.visible");
+            cy.get("@emailInput").should("be.visible");
+            cy.get("@firstNameInput").should("be.visible");
+            cy.get("@lastNameInput").should("be.visible");
+            cy.get("@passwordInput").should("be.visible");
+            cy.get("@formSubmitBtn").should("be.visible");
         });
 
         it("should submit signup form successfully", () => {
-            cy.get("[data-test='signup-button']").click();
-            cy.get("[data-test='signup-email-input']").type("akash@gmail.com");
-            cy.get("[data-test='signup-firstname-input']").type("Akash");
-            cy.get("[data-test='signup-lastname-input']").type("Das");
-            cy.get("[data-test='signup-password-input']").type("Testing123");
-            cy.get("[data-test='signup-form']").submit();
+            cy.get("@emailInput").type("akash@gmail.com");
+            cy.get("@firstNameInput").type("Akash");
+            cy.get("@lastNameInput").type("Das");
+            cy.get("@passwordInput").type("Testing123");
+            cy.get("@formSubmitBtn").click();
+        });
+
+        it("should show error message when signup form is submitted with invalid data", () => {
+            cy.get("@emailInput").type("invalid-email");
+            cy.get("@firstNameInput").type("Akash");
+            cy.get("@lastNameInput").type("Das");
+            cy.get("@passwordInput").type("Testing123");
+            cy.get("@formSubmitBtn").click();
+
+            cy.get("[data-test='signup-email-input'] > .helper-text").should(
+                "be.visible"
+            );
         });
     });
 });
